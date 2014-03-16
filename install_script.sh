61e71d438491fd9d1d996fb91e04187a671d3718
Gregory S. Kielian <gregory.kielian@gmail.com>
2014-03-16 16:53:18 -0700

    Create install_script.sh

---
+++ install_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# I used the following webcam and it works without need to add drivers
+# Logitech HD Webcam C270, 720p Widescreen Video Calling and Recording
+
+sudo apt-get install -y ffmpeg
+sudo apt-get install -y mplayer
+sudo apt-get install -y flac
