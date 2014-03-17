cfb638c64fd4a64b151fb31ed671b173be2f92e4
gskielian <gregory.kielian@gmail.com>
2014-03-17 01:36:39 -0700

    added raspberry pi say.sh

--- raspi_say.sh
+++ raspi_say.sh
@@ -3,7 +3,6 @@
 # I used the following webcam and it works without need to add drivers
 # Logitech HD Webcam C270, 720p Widescreen Video Calling and Recording
 
-
 duration=$1
 
 echo "Beginning Recording"
@@ -15,13 +14,19 @@ if [ -f file.flac ] ; then
 rm file.flac
 fi
 
-arecord -f S16_LE -d $duration file.wav  -D "hw:0,0"  -r 16000 -c2
 
+echo "removing previous files"
+rm file.wav
+rm file.flac
 
+#arecord -f S16_LE -d $duration file.wav  -D "hw:0,0"  -r 16000 -c2
+
+arecord -f S16_LE -d $duration file.wav -D "plughw:1,0" -r 16000 -c2
+echo "ffmpeg processing"
 ffmpeg -loglevel panic -y -i file.wav -ar 16000 -acodec flac file.flac  > /dev/null 2>&1
 
+echo "Sending to Cloud"
 
-echo "Processing..."
 curl -X POST \
 --data-binary @file.flac \
 --user-agent 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36' \
