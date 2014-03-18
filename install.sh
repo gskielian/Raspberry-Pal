39d2c777f0948dfb51e36c50a0ab07167f5206ca
gskielian <gregory.kielian@gmail.com>
2014-03-18 01:13:33 -0700

    created useful_things.sh, decided to just make jq

--- install.sh
+++ install.sh
@@ -7,3 +7,7 @@ sudo apt-get install -y ffmpeg
 sudo apt-get install -y mplayer
 sudo apt-get install -y flac
 sudo apt-get install -y jq # like sed for json
+wget http://stedolan.github.io/jq/download/source/jq-1.3.tar.gz
+tar -zxvf jq-1.3.tar.gz
+cd jq-1.3
+./configure && make && sudo make install
