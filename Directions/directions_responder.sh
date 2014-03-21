74e30d245d147db7f8390d6765caabd47919b365
gskielian <gregory.kielian@gmail.com>
2014-03-20 23:55:49 -0700

    added comments to directions_responder.sh

--- Directions/directions_responder.sh
+++ Directions/directions_responder.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-response=`cat newfile | jq '.routes[0].legs[0].steps[0].html_instructions'  | sed 's/<[^>]*>//g'`
+response=`cat googlemaps_api_response.json \
+| jq '.routes[0].legs[0].steps[0].html_instructions' \
+| sed 's/<[^>]*>//g'` #removes html tags
 
-bash ./say.sh "$response"
+bash ./say.sh "$response" #returns clear version of directions
+
+#Todo: Rephrase the response from the Google Maps API
+#       to sound less like a GPS Navigation robot
--- Directions/directions_responsder.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-response=`cat newfile | jq '.routes[0].legs[0].steps[0].html_instructions'  | sed 's/<[^>]*>//g'`
-
-bash ./say.sh "$response"
